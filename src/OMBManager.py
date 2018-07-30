656c27ba52d8aa06c4c22e676b34dbf6934aa781
Dimitrij <Dima-73@inbox.lv>
2018-07-30 17:34:28 +0300

    Don't use network drives for OMB

--- src/OMBManager.py
+++ src/OMBManager.py
@@ -230,15 +230,15 @@ def OMBManager(session, **kwargs):
 		session.open(OMBManagerList, OMB_MAIN_DIR)
 		found = True
 	else:
-		for partition in harddiskmanager.getMountedPartitions():
-			if partition.mountpoint != '/':
-				data_dir = partition.mountpoint + '/' + OMB_DATA_DIR
-				if os.path.exists(data_dir) and os.access(partition.mountpoint, os.F_OK|os.R_OK) and isMounted(partition.mountpoint):
+		for p in harddiskmanager.getMountedPartitions():
+			if p.mountpoint != '/' and (p.device[:2] == 'sd' or (p.device.startswith('mmcblk0p') and BOX_NAME not in ('5008', 'et13000', 'et11000',' et1x000', 'uno4k', 'uno4kse', 'ultimo4k', 'solo4k', 'zero4k', 'hd51', 'hd52', 'dm820', 'dm7080', 'sf4008', 'dm900', 'dm920', 'gb7252', 'lunix3-4k', 'vs1500', 'h7', '8100s'))):
+				data_dir = p.mountpoint + '/' + OMB_DATA_DIR
+				if os.path.exists(data_dir) and os.access(p.mountpoint, os.F_OK|os.R_OK) and isMounted(p.mountpoint):
 					#if not os.path.ismount('/usr/lib/enigma2/python/Plugins/Extensions/OpenMultiboot'):
 					#	if os.readlink("/sbin/init") == "/sbin/init.sysvinit":
 					#		if os.path.isfile('open_multiboot'):
 					#			os.system("ln -sfn /sbin/open_multiboot /sbin/init")
-					session.open(OMBManagerList, partition.mountpoint)
+					session.open(OMBManagerList, p.mountpoint)
 					found = True
 					break
 	if not found:
